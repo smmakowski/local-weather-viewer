7411f5f2721ae3084ffac8a3aea6a4ca7bb36254
smmakowski <makowski.sm@gmail.com>
2017-08-14 19:44:50 -0700

    acquire geolocation and weather API json data

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
+  var currPos = {
+    lat: 37.3382,
+    lon: -121.8863,
+  }
+  var baseURL = 'https://fcc-weather-api.glitch.me/api/current?';
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      currPos['lat'] = pos.coords.latitude;
+      currPos['lon'] = pos.coords.longitude;
+      $.getJSON(baseURL + 'lat=' + currPos.lat + '&lon=' + currPos.lon, function(json){
+        console.log(json);
+      });
+    });
 
-	// DELETE CODE IN LINE 4 ENTER YOUR CUSTOM CODE HERE
-  $('#placeholder').css({color: 'green'});
+
+  }
+
 
 });
